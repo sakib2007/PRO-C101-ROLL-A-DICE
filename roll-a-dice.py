18b9ce25d2321d2b18fde6e154f95ff475526190
sakib2007 <74136713+sakib2007@users.noreply.github.com>
2022-12-12 19:51:18 +0530

    Add files via upload

--- roll-a-dice.py
+++ roll-a-dice.py
@@ -1,11 +1,16 @@
 import random 
   
+# Take a variable to store response to keep rolling a dice
 response = "y"
    
 while response == "y": 
       
+    # Gnenerates a random number 
+    # between 1 and 6 (including 
+    # both 1 and 6) 
     no = random.randint(1,6) 
 
+    # conditions to check the number value  
     if no == 1: 
         print("[-----]") 
         print("[     ]") 
@@ -42,6 +47,7 @@ while response == "y":
         print("[     ]") 
         print("[0 0 0]") 
         print("[-----]") 
-              
+          
+    # Ask user to enter a response      
     response=input("press y to roll again and n to exit:") 
     print("\n") 
